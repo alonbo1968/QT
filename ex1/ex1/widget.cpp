55f6d4ab37ddbb08da3fb31e487ded66a1dc854b
Alon <alon_bo@hotmail.comm>
2025-04-30 00:18:19 +0300

    First Qt project

---
+++ ex1/ex1/widget.cpp
@@ -0,0 +1,22 @@
+#include "widget.h"
+#include "ui_widget.h"
+#include <QDebug>
+#include <QMessageBox>
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    ui->setupUi(this);
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
+
+void Widget::on_pushButton_clicked()
+{
+    //qDebug() << "user Clicked on button";
+    QMessageBox::information(this,"Warnning","You clicked",QMessageBox::Abort);
+}
